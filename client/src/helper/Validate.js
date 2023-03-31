2dc66078565164ba90a7d21e37e43567eb5ddf3b
Morad17 <morad123@hotmail.co.uk>
2023-03-31 03:27:31 +0100

    created register page

--- client/src/helper/Validate.js
+++ client/src/helper/Validate.js
@@ -1,11 +1,12 @@
 import toast from "react-hot-toast"
 
+
+//Validate Login details //
 export async function userValidate(values){
     const errors = userVerify({}, values)
 
     return errors
 }
-
 
 function userVerify(error = {}, values){
     if(!values.username){
@@ -16,10 +17,44 @@ function userVerify(error = {}, values){
 
     if(!values.password){
         error.password = toast.error("Password Required")
-    } else if(values.password.include(" ")){
+    } else if(values.password.includes(" ")){
         error.password = toast.error("Wrong Password")
     } else if(values.password.length < 4){
         error.password = toast.error("Password must be longer than 4 characters")
     }
     return error
+}
+
+// Validate Register Details //
+export async function registerValidate(values){
+    const errors = userVerify({}, values)
+    passwordVerify(errors, values)
+    emailVerify(errors, values)
+}
+
+function emailVerify(error = {}, values){
+    if(!values.email){
+        error.email = toast.error("Email Required")
+    }else if(values.email.includes(" ")){
+        error.email = toast.error("Enter a valid Email")
+    }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+        error.email = toast.error("Invalid Email Address")
+    }
+    return error
+}
+
+function passwordVerify(errors = {}, values){
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+    if(!values.password){
+        errors.password = toast.error("Password Required");
+    } else if(values.password.includes(" ")){
+        errors.password = toast.error("Wrong Password");
+    }else if(values.password.length < 4){
+        errors.password = toast.error("Password must be more than 4 characters long");
+    }else if(!specialChars.test(values.password)){
+        errors.password = toast.error("Password must have special character");
+    }
+
+    return errors;
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { Toaster } from 'react-hot-toast'
+import { useFormik } from 'formik'
+import { registerValidate } from '../helper/Validate'
 
 const Register = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: ''
+    },
+    validate: registerValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async values => {
+      console.log(values);
+    }
+  })
+
   return (
-    <div>Register</div>
+    <div>
+      <h1>Hello</h1>
+      <Toaster posisiton="top-center"></Toaster>
+      <div className="login">
+        <p>Register</p>
+        <form onSubmit={formik.handleSubmit}>
+          <div className="">
+            <img src="" alt="avatar" />
+          </div>
+          <div className="textbox">
+          <input {...formik.getFieldProps('email')}type="text" placeholder="email" />
+            <input {...formik.getFieldProps('username')}type="text" placeholder="username" />
+            <input {...formik.getFieldProps('password')}type="text" placeholder="password" />
+            <button type="submit">rRegister</button>
+          </div>
+          
+        </form>
+      </div>
+    </div>
   )
 }
 
